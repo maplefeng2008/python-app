85ddf5700ed8e6e428d4cd70900db8586933c52e
maplefeng2008 <wanglichao2002@sina.com>
2019-07-05 16:49:47 +0800

    v0.1

--- sendMail.py
+++ sendMail.py
@@ -7,15 +7,19 @@ from_addr = 'wanglichao2002@sina.com'
 password = 'adfADF219'
 #to_addr = input('To: ')
 to_addr = 'wlcearth@hotmail.com'
-smtp_server = input('SMTP server: ')
-#smtp_server = 'smtp.sina.com'
+#smtp_server = input('SMTP server: ')
+smtp_server = 'smtp.sina.com'
+
+msg['From'] = from_addr
+msg['To'] = to_addr
+msg['Subject'] = 'python send mail'
 
 import smtplib
 print('connecting to smtp server...')
 server = smtplib.SMTP(smtp_server, 25)
 print('connected!')
-server.set_debuglevel(1)
+#server.set_debuglevel(1)
 server.login(from_addr, password)
-server.sendmail(from_addr, [to_addr], msg.as_string())
+server.sendmail(from_addr, to_addr, msg.as_string())
 print('send mail complete!')
 server.quit()
