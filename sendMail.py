4929602d06c901153ba94c172332a2b16b95c57f
maplefeng2008 <wanglichao2002@sina.com>
2019-07-05 17:39:05 +0800

    v0.3

--- sendMail.py
+++ sendMail.py
@@ -1,13 +1,13 @@
 from email import encoders
 from email.header import Header
 from email.mime.text import MIMEText
-from email.utils import parseaddr, fromataddr
+from email.utils import parseaddr, formataddr
 
 import smtplib
 
 def _format_addr(s):
     name, addr = parseaddr(s)
-    return formataddr(Header(name, 'utf-8').encode(), addr)
+    return formataddr((Header(name, 'utf-8').encode(), addr))
 
 #from_addr = input('From: ')
 from_addr = 'wanglichao2002@sina.com'
@@ -18,18 +18,22 @@ to_addr = 'wlcearth@hotmail.com'
 #smtp_server = input('SMTP server: ')
 smtp_server = 'smtp.sina.com'
 
-msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+#msg = MIMEText('hello, send by Python...3', 'plain', 'utf-8')
+msg = MIMEText('<html><body><hi>Hello</h1>' +
+        '<p>send by <a href="https://github.com/maplefeng2008">wang</a>...</p>' +
+        '</body></html>', 'html', 'utf-8')
+
 #msg['From'] = from_addr
 msg['From'] = _format_addr('wanglc <%s>' % from_addr)
 #msg['To'] = to_addr
 msg['To'] = _format_addr('haha <%s>' % to_addr)
 #msg['Subject'] = 'python send mail'
-msg['Subject'] = Header(' python send mail', 'utf-8').encode()
+msg['Subject'] = Header(' python send mail4', 'utf-8').encode()
 
 print('connecting to smtp server...')
 server = smtplib.SMTP(smtp_server, 25)
 print('connected!')
-#server.set_debuglevel(1)
+server.set_debuglevel(1)
 server.login(from_addr, password)
 server.sendmail(from_addr, to_addr, msg.as_string())
 print('send mail complete!')
