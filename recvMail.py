2a3e04dde7202b31ee2a1d1764369c912f193704
maplefeng2008 <wanglichao2002@sina.com>
2019-07-08 14:28:42 +0800

    fix bug

--- recvMail.py
+++ recvMail.py
@@ -1,15 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from email.parser import Parser
+from email.header import decode_header
+from email.utils import parseaddr
 import poplib
 
 #email = input('Email: ')
-email = 'wlcearth@hotmail.com'
+#email = 'wlcearth@hotmail.com'
+email = 'wanglichao2002@sina.com'
 #password = input('Password: ')
-password = 'adfadf219'
+#password = 'adfadf219'
+password = 'adfADF219'
 #pop3_server = input('POP3 server: ')
-pop3_server = 'outlook.office365.com'
+#pop3_server = 'outlook.office365.com'
+pop3_server = 'pop.sina.com'
+
+def guess_charset(msg):
+    charset = msg.get_charset()
+    if charset is None:
+        content_type = msg.get('Content-Type', '').lower()
+        pos = content_type.find('charset=')
+        if pos >= 0:
+            charset = content_type[pos + 8:].strip()
+    return charset
+
+def decode_str(s):
+    value, charset = decode_header(s)[0]
+    if charset:
+        value = value.decoder(charset)
+    return value
+
+def print_info(msg, indent=0):
+    if indent == 0:
+        for header in ['From', 'To', 'Subject']:
+            value = msg.get(header, '')
+            if value:
+                if header == 'Subject':
+                    value = decode_str(value)
+                else:
+                    hdr, addr = parseaddr(value)
+                    name = decode_str(hdr)
+                    value = u'%s <%s>' % (name, addr)
+            print('%s%s: %s' % ('  ' * indent, header, value))
+    if (msg.is_multipart()):
+        parts = msg.get_payload()
+        for n, part in enumerate(parts):
+            print('%s part %s' % ('  ' * indent, n))
+            print('%s----------------------' % ('  ' * indent))
+            print_info(part, indent + 1)
+    else:
+        content_type = msg.get_content_type()
+        if content_type=='text/plain' or content_type=='text/html':
+            content = msg.get_payload(decode=True)
+            charset = guess_charset(msg)
+            if charset:
+                content = content.decode(charset)
+            print('%s Text: %s' % ('  ' * indent, content + '...'))
+        else:
+            print('%s Attachment: %s' % ('  ' * indent, content_type))
 
 server = poplib.POP3_SSL(pop3_server, port=995)
-server.set_debuglevel(1)
+#server.set_debuglevel(1)
 print(server.getwelcome().decode('utf-8'))
 
 server.user(email)
@@ -24,5 +76,8 @@ resp, lines, octets = server.retr(index)
 
 msg_content = b'\r\n'.join(lines).decode('utf-8')
 msg = Parser().parsestr(msg_content)
+print_info(msg)
 
+#delete mail from server by index ID
+#server.dele(index)
 server.quit()
